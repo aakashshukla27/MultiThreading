0e1bbfef05bae77129d1335fb45b3824286b2d3d
Aakash Shukla <shukla.aa@northeastern.edu>
2021-08-26 17:48:18 -0400

    Added Task Chaining

---
+++ TaskChaining/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace TaskChaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<string> antecedent = Task.Run(()
+                =>
+                {
+                    Task.Delay(10000);
+                    return DateTime.Today.ToShortDateString();
+                });
+            Task<string> continuation = antecedent.ContinueWith(x => "Today is " + antecedent.Result);
+            Console.WriteLine("This will display before result");
+            Console.WriteLine(continuation.Result);
+        }
+    }
+}
